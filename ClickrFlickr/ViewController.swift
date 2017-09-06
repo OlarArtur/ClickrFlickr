dc42398ee14d49c77bd34f5f1558d7f27bcad61c
Artur Olar <artur_olar@epbygrow0152.grodno.epam.com>
2017-09-06 18:24:21 +0300

    Initial Commit

---
+++ ClickrFlickr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClickrFlickr
+//
+//  Created by Artur Olar on 9/6/17.
+//  Copyright © 2017 Artur Olar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
