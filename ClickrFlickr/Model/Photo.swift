ee9875453c1b37b7abd94b6397748ee5001e5e0d
Artur Olar <artur_olar@epbygrow0152.grodno.epam.com>
2017-10-30 11:46:36 +0300

    Add Photo Struct

---
+++ ClickrFlickr/Model/Photo.swift
@@ -0,0 +1,23 @@
+//
+//  Photo.swift
+//  ClickrFlickr
+//
+//  Created by Artur Olar on 10/30/17.
+//  Copyright © 2017 Artur Olar. All rights reserved.
+//
+
+import UIKit
+
+
+struct Photo {
+    
+    let title: String
+    let farm: String
+    let server: String
+    let id: String
+    let secret: String
+    
+    var url: String {
+        return "https://farm\(farm).staticflickr.com/\(server)/\(id)_\(secret).jpg"
+    }
+}
